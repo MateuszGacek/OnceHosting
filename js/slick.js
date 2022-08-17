b4a74b2a90d93b1c9435c87554338532c5ac257a
Mateusz <mateusz_gacek@interia.pl>
2022-08-17 20:24:00 +0200

    add about us, pricing and implementation slick slider

---
+++ js/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$(".pricing__box").slick({
+		prevArrow: false,
+		nextArrow: false,
+		slidesToShow: 3,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					infinite: true,
+					initialSlide: 1,
+				},
+			},
+		],
+	});
+});
