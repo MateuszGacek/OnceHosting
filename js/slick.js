9ab7d3b0fd6a7b575c1bd09ffc356724bdaefbbc
Mateusz <mateusz_gacek@interia.pl>
2022-08-18 20:29:46 +0200

    added section support and feedback, added footer

--- js/slick.js
+++ js/slick.js
@@ -17,3 +17,30 @@ $(document).ready(function () {
 		],
 	});
 });
+
+$(document).ready(function () {
+	$(".feedback__box").slick({
+		prevArrow: false,
+		nextArrow: false,
+		slidesToShow: 3,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 650,
+				settings: {
+					slidesToShow: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+		],
+	});
+});
