35932df0a02695d587690379bcba085983a063d2
Mateusz <mateusz_gacek@interia.pl>
2022-10-18 14:22:52 +0200

    remove main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// $(window).scroll(function () {
-// 	$("#foot").removeClass("view-footer");
-// 	if ($(window).scrollTop() + $(window).height() > $(document).height() - 40) {
-// 		//you are at bottom
-// 		$("#foot").addClass("view-footer");
-// 	}
-// });
